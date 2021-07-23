881f3fd48feedf4e5c6bb2ac29535f4e47acf248
Adrian Marimon <marimon.dev@Adrians-MacBook-Pro.local>
2021-07-23 16:53:42 -0400

    fixed

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Books
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
@@ -46,5 +46,6 @@ module Books
           methods: [:get, :patch, :put, :delete, :post, :options]
           )
       end
+    end
   end
 end
